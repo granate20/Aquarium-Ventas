bf9bc866beebbacd52b4acf3387e750896b68fc3
granate20@gmail.com <granate20@gmail.com>
2015-01-13 19:30:57 -0300

    sistema login y conexion
    Se agrega login y controlador de conexion

--- js/index.js
+++ js/index.js
@@ -1,49 +1,33 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+// JavaScript Document
+function checkConnection() { 
+    var states = {}; 
+    states[Connection.UNKNOWN]  = 'Unknown connection'; 
+    states[Connection.ETHERNET] = 'Ethernet connection'; 
+    states[Connection.WIFI]     = 'WiFi connection'; 
+    states[Connection.CELL_2G]  = 'Cell 2G connection'; 
+    states[Connection.CELL_3G]  = 'Cell 3G connection'; 
+    states[Connection.CELL_4G]  = 'Cell 4G connection'; 
+    states[Connection.CELL]     = 'Cell generic connection'; 
+    states[Connection.NONE]     = 'No network connection'; 
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+} 
 
-        console.log('Received Event: ' + id);
-    }
-};
+var networkState; 
+
+var app = { 
+    initialize: function() { 
+        this.bindEvents(); 
+    }, 
+    bindEvents: function() { 
+        document.addEventListener('deviceready', this.onDeviceReady, false); 
+    }, 
+    onDeviceReady: function() { 
+        networkState = navigator.connection.type; 
+        checkConnection(); 
+		if(networkState=='No network connection') alert("NO HAY CONEXION A INTERNET");
+		
+		$(document).ready(function(e) {   
+							  $("div:jqmData(role='panel')").css('margin-top',  ($("div:jqmData(role='header')").height()));   
+							}); 
+    } 
+}; 
