34276c2674c93847791deacfe25c971076e09484
granate20@gmail.com <granate20@gmail.com>
2015-01-29 20:06:15 -0300

    Se agrega pedidos

--- js/index.js
+++ js/index.js
@@ -46,6 +46,34 @@ function connect()
 			
 			} 
 			});} 
+
+
+function verpendientes() 
+			{ 
+			$.ajax({ 
+			url:'http://mxxiv.com.ar/aqm/pendientes.php?jsoncallback=?', 
+			type:'POST', 
+			data:{pk_usuario:usuario.pk_usuario}, 
+			dataType:'json', 
+			error:function(jqXHR,text_status,strError){ 
+			alert("Sin Conexión");}, 
+			timeout:60000, 
+			success:function(data){ 
+				
+				$("#result").html('');
+			
+			for(var i in data){ 
+			
+				
+				$("#result").append('<li><a href=""  style="height:50px" class="ui-btn ui-btn-icon-right ui-icon-carat-r">'+data[i].nombre+' Tel:'+data[i].telefono+'</a></li>');
+				
+			} 
+			
+			
+			} 
+			});} 
+
+
 			
 			
 function agregarcliente()
@@ -93,10 +121,12 @@ var app = {
         tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
 
 
-          tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+          tx.executeSql("select id as cnt from test_table;", [], function(tx, res) {
             if(res.rows.item(0).cnt>0) 
 			{
 				$.mobile.changePage("#home");
+				
+				usuario.pk_usuario=res.rows.item(0).cnt;
 							
 				var page = $('[data-role="page"]:last'); 
     			page.find('#divcli').hide();
